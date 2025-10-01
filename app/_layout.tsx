89349eadcfc9867a3e8278324e5d585d33b9816d
Jake Drzewiecki <jakedrz@Drzewieckis-MacBook-Pro.local>
2025-09-30 20:33:46 -0500

    started adding tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <NativeTabs>
+        <NativeTabs.Trigger name="index">
+          <Label>Home</Label>
+          <Icon sf="figure.walk" drawable="custom_android_drawable" />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="games">
+          <Icon sf="trophy.fill" drawable="custom_settings_drawable" />
+          <Label>Games</Label>
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="profile">
+          <Icon sf="person.crop.circle.fill" drawable="custom_settings_drawable" />
+          <Label>Profile</Label>
+        </NativeTabs.Trigger>
+      </NativeTabs>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View, PlatformColor, Appearance } from "react-native";
 
 export default function Index() {
+    Appearance.setColorScheme("dark");
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: PlatformColor("systemBackground")
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+      style={{color: PlatformColor("label")}}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
