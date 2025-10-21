f665a6c0e6badda8f836224941c708cedb7cfb77
Jake Drzewiecki <jake@Courtneys-MacBook-Pro.local>
2025-10-21 16:23:49 -0500

    added blank cards to games page

--- app/games/index.jsx
+++ app/games/index.jsx
@@ -3,8 +3,30 @@ import {PlatformColor, ScrollView, Text, View} from "react-native";
 export default function Index() {
   return (
         <ScrollView contentInsetAdjustmentBehavior="never"
+                    contentContainerStyle={{
+                        alignItems: 'center',
+                    }
+                    }
         style={{
             backgroundColor: PlatformColor('systemBackground'),
-        }}><Text style={{fontSize: 87, color: PlatformColor('label')}}>Edit app/games.tsx to edit this screen.</Text></ScrollView>
+        }}>
+            <GameCard/>
+            <GameCard/>
+            <GameCard/>
+            <GameCard/>
+        </ScrollView>
   );
+}
+
+const GameCard = () => {
+    return (
+        <View style={{
+            width: '90%',
+            height: '200',
+            backgroundColor: PlatformColor('secondarySystemBackground'),
+            borderRadius: 10,
+            padding: 20,
+            marginVertical: 10,
+        }}></View>
+    )
 }
