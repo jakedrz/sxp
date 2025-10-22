48c064c1d5dfc7e05573daff9d72f2b762874831
Jake Drzewiecki <jakedrz@Drzewieckis-MacBook-Pro.local>
2025-10-22 00:27:35 -0500

    added join game button

--- app/games/index.jsx
+++ app/games/index.jsx
@@ -1,4 +1,13 @@
-import {PlatformColor, ScrollView, Text, View, Button} from "react-native";
+import {
+    PlatformColor,
+    ScrollView,
+    Text,
+    View,
+    Button,
+    Touchable,
+    TouchableHighlight,
+    DynamicColorIOS
+} from "react-native";
 import {SymbolView, SymbolWeight} from 'expo-symbols';
 
 export default function Index() {
@@ -25,7 +34,7 @@ const GameCard = () => {
             width: '90%',
             minHeight: '200',
             backgroundColor: PlatformColor('secondarySystemBackground'),
-            borderRadius: 10,
+            borderRadius: 20,
             padding: 20,
             marginVertical: 10,
             borderCurve:'continuous'
@@ -64,28 +73,61 @@ const GameCard = () => {
                 flexDirection: 'row',
                 justifyContent: 'space-between'
             }}>
-                <View style={{flexDirection:'row', alignItems: 'center',}}>
+                <View style={{flexDirection:'column', alignItems: 'center',}}>
                     <SymbolView name='dollarsign.circle'
-                    tintColor={PlatformColor('tertiaryLabel')}/><Text style={{
+                    tintColor={PlatformColor('tertiaryLabel')}
+                    size={32}/><Text style={{
                         color: PlatformColor('tertiaryLabel'),
+                    fontSize: 16,
                     }}>$40 bet</Text>
                 </View>
-                <View style={{flexDirection:'row', alignItems: 'center'}}>
+                <View style={{flexDirection:'column', alignItems: 'center'}}>
                     <SymbolView name='person.3.fill'
+                                size={32}
                                 tintColor={PlatformColor('tertiaryLabel')}/><Text style={{
                         color: PlatformColor('tertiaryLabel'),
+                        fontSize: 16,
                     }}>647 Players</Text>
                 </View>
-                <View style={{flexDirection:'row', alignItems: 'center'}}>
+                <View style={{flexDirection:'column', alignItems: 'center'}}>
                     <SymbolView name='trophy.circle'
+                                size={32}
                                 tintColor={PlatformColor('tertiaryLabel')}/><Text style={{
+                                    fontSize: 16,
                         color: PlatformColor('tertiaryLabel'),
                     }}>$26,380 pot</Text>
                 </View>
             </View>
-            <View style={{alignSelf: 'flex-end'}}>
-            <Button title="Join Game"/>
+            <View style={{marginTop: 20}}>
+                <JoinButton/>
             </View>
         </View>
+    )
+}
+const color = DynamicColorIOS({
+dark: '#A18ADF',
+    highContrastDark: '#a891e7'
+})
+
+
+const JoinButton = () => {
+
+    return (
+        <TouchableHighlight
+        underlayColor="white"
+        style={{borderRadius: 30, borderCurve:'continuous'}}
+        onPress={() => console.log('hi')}>
+            <View style={{
+            backgroundColor:PlatformColor('tertiarySystemBackground'),
+                minHeight: 40,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius:30,
+                borderCurve:'continuous',
+        }}>
+            <Text style={{color: color,
+            fontSize: 18, fontWeight:500}}>Join Game</Text>
+            </View>
+        </TouchableHighlight>
     )
 }
